b8900f0dc549362138a2bc0d60774f07ee173670
Nate Lumpkin <nate.lumpkin@gmail.com>
2022-09-22 14:30:06 -0700

    Added user authentication express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,40 @@
 const router = require('express').Router();
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
-router.post('/test', (req, res) => {
-  res.json({requestBody:req.body});
-});
+// all Route handlers will retrieve the current user on the req obj
+// as req.user
+// otherwise, set req.user to null
+router.use(restoreUser);
+
+// Below are old routehandlers for testing the authorization middleware
+// See Authorize Me Readme Phase 3
+
+// ------------ START OLD TESTS --------
+
+// router.post('/test', (req, res) => {
+//   res.json({requestBody:req.body});
+// });
+
+// router.get('/set-token-cookie', async (req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'bigMagorgus'
+//     }
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({user});
+// });
+
+// router.get('/restore-user', (req, res) => {
+//   return res.json(req.user);
+// });
+
+// router.get('/require-auth', requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
+
+// ------- END OLD TESTS -------
 
 
 
