bcf36f7507583484cb66ae4eb8df8826357c935f
Nate Lumpkin <nate.lumpkin@gmail.com>
2022-09-25 00:45:20 -0700

    added basic seeder for spots

--- backend/db/seeders/20220925073346-spot-seed.js
+++ backend/db/seeders/20220925073346-spot-seed.js
@@ -11,6 +11,32 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    await  queryInterface.bulkInsert('Spots',[
+      {
+        ownerId: 4,
+        address: '100 California Ave',
+        city: 'Smalltown',
+        state: 'CA',
+        country: 'USA',
+        lat: 31.1194890,
+        lng: -90.1295419,
+        name: 'Smallest House',
+        description: 'The smallest house in the whole world',
+        price: 120.99
+      },
+      {
+        ownerId: 5,
+        address: '200 Michaels St',
+        city: 'Bigville',
+        state: 'NY',
+        country: 'United States of Ameria',
+        lat: -61.1194890,
+        lng: 90.1295419,
+        name: 'Biggest House',
+        description: 'The biggest house in the whole world',
+        price: 1120.99
+      }
+    ])
   },
 
   async down (queryInterface, Sequelize) {
@@ -20,5 +46,10 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('Users', {
+      name: {
+        [Op.in]: ['Smallest House','Biggest House']
+      }
+    })
   }
 };
