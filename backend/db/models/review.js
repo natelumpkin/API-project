5b82c059a4c421250524b0b1a8e33e99ea1529f1
Nate Lumpkin <nate.lumpkin@gmail.com>
2022-09-25 11:56:18 -0700

    added review model and migrations

---
+++ backend/db/models/review.js
@@ -0,0 +1,53 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Review.belongsTo(
+        models.Spot,
+        { foreignkey: 'spotId' }
+      )
+      Review.belongsTo(
+        models.User,
+        { foreignKey: 'userId' }
+      )
+    }
+  }
+  Review.init({
+    spotId: {
+      type: DataTypes.INTEGER
+    },
+    userId: {
+      type:DataTypes.INTEGER
+    },
+    review: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    stars: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 5
+      }
+    },
+    createdAt: {
+      type: DataTypes.DATE
+    },
+    updatedAT: {
+      type: DataTypes.DATE
+    }
+  }, {
+    sequelize,
+    modelName: 'Review',
+  });
+  return Review;
+};
