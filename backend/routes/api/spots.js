edbd157b1350eb31b9018563ca969f6a4df63646
Nate Lumpkin <nate.lumpkin@gmail.com>
2022-09-26 17:06:02 -0700

    modifying get all spots to solve postgres bug

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
         [sequelize.fn('AVG',sequelize.col('stars')),'avgRating'],
       ]
     },
-    group: "Spot.id",
+    group: ['Spot.id','SpotImages.id'],
     include: [{
       model: SpotImage,
     },
