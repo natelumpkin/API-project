2f7d88d315304722cc642f663ca4756d378d7893
Nate Lumpkin <nate.lumpkin@gmail.com>
2022-10-13 21:38:39 -0700

    frontend authme phase 0 complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,12 +4,35 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-TOKEN': csrfToken
+if (process.env.NODE_ENV !== 'production') {
+  router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-TOKEN': csrfToken
+    });
   });
-});
+}
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  router.use(express.static(path.resolve('../frontend/build')));
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+};
+
+
 
 module.exports = router;
