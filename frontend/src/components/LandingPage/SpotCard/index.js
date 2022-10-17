d1a0b9128ba99d62cbf1cff9e67efd3523061629
Nate Lumpkin <nate.lumpkin@gmail.com>
2022-10-17 08:31:13 -0700

    fixed preview url bug in get all spots backend

--- frontend/src/components/LandingPage/SpotCard/index.js
+++ frontend/src/components/LandingPage/SpotCard/index.js
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 
 const SpotCard = ({ spot }) => {
   // console.log('SPOT CARD spot PROP: ', spot)
+  console.log('SPOT CARD SPOT: ', spot)
   return (
     <Link to={`/spots/${spot.id}`}>
       <div className="spotcard-top-holder">
         <div className="spotcard-preview-image-holder">
-          <img src={spot.url} alt='preview image of spot'/>
+          <img src={spot.previewImage} alt='preview image of spot'/>
         </div>
       <div className="spotcard-bottom-holder">
         <div>{spot.city},{spot.state}</div>
